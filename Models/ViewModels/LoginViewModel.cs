d50cd5d203c9530265dc33f40bfe851869c3e2b2
Tomek <kemot15@tlen.pl>
2020-05-12 14:29:49 +0200

    add registration, login

---
+++ Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotorGliding.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Email jest wymagany")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Hasło jest wymagane")]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+    }
+}
---
+++ Models/ViewModels/RegistrationViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MotorGliding.Models.ViewModels
+{
+    public class RegistrationViewModel
+    {
+        [Required(ErrorMessage = "Pole wymagane")]
+        public string UserName { get; set; }
+        [Required(ErrorMessage = "Pole wymagane")]
+        [EmailAddress(ErrorMessage = "Nie poprawny adres email")]
+        public string Email { get; set; }
+        [Required(ErrorMessage = "Pole wymagane")]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+        [Required(ErrorMessage = "Pole wymagane"), Compare("Password")]
+        [PasswordPropertyText]
+        public string RepeatPassword { get; set; }
+    }
+}
